d0ea587131b481a4ca43a87b4f9c5f00e792d3c0
Josue <22620229@tlaxiaco.tecnm.mx>
2024-05-29 11:59:29 -0600

    primera actualizacion

---
+++ Practica2/IngresarDatos.Designer.cs
@@ -0,0 +1,298 @@
+﻿namespace Practica2
+{
+    partial class IngresarDatos
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(IngresarDatos));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.Regresar2 = new System.Windows.Forms.Button();
+            this.Nombre = new System.Windows.Forms.TextBox();
+            this.Curp = new System.Windows.Forms.TextBox();
+            this.Apellido = new System.Windows.Forms.TextBox();
+            this.Edad = new System.Windows.Forms.TextBox();
+            this.Telefono = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Continuar = new System.Windows.Forms.Button();
+            this.Salir = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.SkyBlue;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.flowLayoutPanel1);
+            this.panel1.Location = new System.Drawing.Point(-2, -2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(975, 78);
+            this.panel1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Practica2.Properties.Resources.descargar;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(98, 67);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(272, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(240, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Ingrese Sus Datos";
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(252, 22);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(7, 6);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.LightSkyBlue;
+            this.panel2.Controls.Add(this.Regresar2);
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(161, 592);
+            this.panel2.TabIndex = 1;
+            // 
+            // Regresar2
+            // 
+            this.Regresar2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Regresar2.Location = new System.Drawing.Point(-2, 522);
+            this.Regresar2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Regresar2.Name = "Regresar2";
+            this.Regresar2.Size = new System.Drawing.Size(166, 53);
+            this.Regresar2.TabIndex = 12;
+            this.Regresar2.Text = "Regresar";
+            this.Regresar2.UseVisualStyleBackColor = true;
+            this.Regresar2.Click += new System.EventHandler(this.Regresar2_Click);
+            // 
+            // Nombre
+            // 
+            this.Nombre.Location = new System.Drawing.Point(233, 206);
+            this.Nombre.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Nombre.Name = "Nombre";
+            this.Nombre.Size = new System.Drawing.Size(192, 22);
+            this.Nombre.TabIndex = 2;
+        
+            // 
+            // Curp
+            // 
+            this.Curp.Location = new System.Drawing.Point(508, 298);
+            this.Curp.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Curp.Name = "Curp";
+            this.Curp.Size = new System.Drawing.Size(192, 22);
+            this.Curp.TabIndex = 3;
+         
+            // 
+            // Apellido
+            // 
+            this.Apellido.Location = new System.Drawing.Point(508, 206);
+            this.Apellido.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Apellido.Name = "Apellido";
+            this.Apellido.Size = new System.Drawing.Size(192, 22);
+            this.Apellido.TabIndex = 4;
+           
+            // 
+            // Edad
+            // 
+            this.Edad.Location = new System.Drawing.Point(233, 298);
+            this.Edad.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Edad.Name = "Edad";
+            this.Edad.Size = new System.Drawing.Size(192, 22);
+            this.Edad.TabIndex = 5;
+            
+            // 
+            // Telefono
+            // 
+            this.Telefono.Location = new System.Drawing.Point(233, 384);
+            this.Telefono.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Telefono.Name = "Telefono";
+            this.Telefono.Size = new System.Drawing.Size(192, 22);
+            this.Telefono.TabIndex = 6;
+      
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(229, 166);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 23);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Nombre(s):";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(504, 166);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 23);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Apellidos:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(229, 267);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 23);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Edad:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(504, 267);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(58, 23);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Curp:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(228, 349);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(192, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Numero de Telefono:";
+            // 
+            // Continuar
+            // 
+            this.Continuar.Location = new System.Drawing.Point(364, 522);
+            this.Continuar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Continuar.Name = "Continuar";
+            this.Continuar.Size = new System.Drawing.Size(142, 44);
+            this.Continuar.TabIndex = 8;
+            this.Continuar.Text = "Continuar";
+            this.Continuar.UseVisualStyleBackColor = true;
+            this.Continuar.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Salir
+            // 
+            this.Salir.Location = new System.Drawing.Point(717, 522);
+            this.Salir.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Salir.Name = "Salir";
+            this.Salir.Size = new System.Drawing.Size(188, 45);
+            this.Salir.TabIndex = 12;
+            this.Salir.Text = "Salir";
+            this.Salir.UseVisualStyleBackColor = true;
+            this.Salir.Click += new System.EventHandler(this.Salir_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(508, 384);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(192, 24);
+            this.progressBar1.TabIndex = 13;
+            // 
+            // IngresarDatos
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(970, 585);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Salir);
+            this.Controls.Add(this.Continuar);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Telefono);
+            this.Controls.Add(this.Edad);
+            this.Controls.Add(this.Apellido);
+            this.Controls.Add(this.Curp);
+            this.Controls.Add(this.Nombre);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "IngresarDatos";
+            this.Text = "Form3";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.TextBox Nombre;
+        private System.Windows.Forms.TextBox Curp;
+        private System.Windows.Forms.TextBox Apellido;
+        private System.Windows.Forms.TextBox Edad;
+        private System.Windows.Forms.TextBox Telefono;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button Regresar2;
+        private System.Windows.Forms.Button Continuar;
+        private System.Windows.Forms.Button Salir;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ Practica2/IngresarDatos.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace Practica2
+{
+    public partial class IngresarDatos : Form
+    {
+        private int fichaNumero = 0;
+        public IngresarDatos()
+        {
+            InitializeComponent();
+            // Ocultar la barra de progreso al inicio
+            progressBar1.Visible = false;
+        }
+
+        static String conexion = "server=localhost:3307;PORT=3307;DATABASE= fichas;UID=root;PASSWORDS=;";
+        MySqlConnection cn = new MySqlConnection(conexion);
+        private string nombre;
+        private string apellido;
+        private string edad;
+        private string curp;
+        private string telefono;
+
+        private void Regresar2_Click(object sender, EventArgs e)
+        {
+            Area formularioArea = new Area();
+
+
+            formularioArea.Show();
+
+
+            this.Close();
+        }
+
+        private void Salir_Click(object sender, EventArgs e)
+        {
+
+            DialogResult resultado = MessageBox.Show("¿Estás seguro de que quieres salir?", "Confirmar salida", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+
+
+            if (resultado == DialogResult.Yes)
+            {
+
+                Application.Exit();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Mostrar la barra de progreso al hacer clic en button2
+            progressBar1.Visible = true;
+            // Aquí puedes agregar la funcionalidad del primer formulario
+            Thread hiloProceso = new Thread(ProcesoLargo);
+            hiloProceso.Start();
+        }
+
+        private void ProcesoLargo()
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                this.Invoke((MethodInvoker)delegate {
+                    progressBar1.Value = i;
+                });
+                Thread.Sleep(50);
+            }
+            // Incrementar el número de ficha al completarse la barra de progreso
+            fichaNumero++;
+            GuardarDatosEnBaseDeDatos();  // Llama al método para guardar los datos en la base de datos
+            this.Invoke((MethodInvoker)delegate {
+                // Mostrar mensaje de éxito al completarse la barra de progreso
+                MessageBox.Show($"Tu número de ficha es: {fichaNumero}.", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            });
+        }
+
+        private void GuardarDatosEnBaseDeDatos()
+        {
+            // Crear la conexión
+            using (MySqlConnection connection = new MySqlConnection(conexion))
+            {
+                try
+                {
+                    // Abrir la conexión
+                    connection.Open();
+
+                    // Preparar la consulta SQL
+                    string query = "INSERT INTO fichas (nombre, apellido, edad, curp, telefono) VALUES (@nombre, @apellido, @edad, @curp, @telefono)";
+
+                    // Crear el comando SQL con la consulta y la conexión
+                    using (MySqlCommand cmd = new MySqlCommand(query, connection))
+                    {
+                        // Agregar los parámetros a la consulta
+                        cmd.Parameters.AddWithValue("@nombre", nombre);
+                        cmd.Parameters.AddWithValue("@apellido", apellido);
+                        cmd.Parameters.AddWithValue("@edad", edad);
+                        cmd.Parameters.AddWithValue("@curp", curp);
+                        cmd.Parameters.AddWithValue("@telefono", telefono);
+
+                        // Ejecutar la consulta
+                        cmd.ExecuteNonQuery();
+                    }
+                }
+                catch (Exception ex)
+                {
+                    // Manejar cualquier error que pueda ocurrir durante el proceso de guardar
+                    this.Invoke((MethodInvoker)delegate {
+                        MessageBox.Show("Error al guardar los datos: " + ex.Message);
+                    });
+                }
+            }
+        }
+
+        // Asignar los eventos TextChanged para actualizar las variables
+        private void textBoxNombre_TextChanged(object sender, EventArgs e)
+        {
+            nombre = ((TextBox)sender).Text;
+        }
+
+        private void textBoxApellido_TextChanged(object sender, EventArgs e)
+        {
+            apellido = ((TextBox)sender).Text;
+        }
+
+        private void textBoxEdad_TextChanged(object sender, EventArgs e)
+        {
+            edad = ((TextBox)sender).Text;
+        }
+
+        private void textBoxCurp_TextChanged(object sender, EventArgs e)
+        {
+            curp = ((TextBox)sender).Text;
+        }
+
+        private void textBoxTelefono_TextChanged(object sender, EventArgs e)
+        {
+            telefono = ((TextBox)sender).Text;
+        }
+    }
+}
